30a87ff8f993642903f9da4cae50da5bb0726fa6
MITRAIS\Agus_M7234 <agus@techniquedm.com>
2018-02-27 16:51:42 +0700

    add docker compose

--- redis.go
+++ redis.go
@@ -3,7 +3,7 @@ package main
 import "github.com/garyburd/redigo/redis"
 
 func NewRedis() redis.Conn {
-	c, err := redis.Dial("tcp", ":6379")
+	c, err := redis.Dial("tcp", "redis:6379")
 	HandleError(err)
 	return c
 }
